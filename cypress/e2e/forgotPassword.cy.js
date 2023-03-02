f9fb5c01444b866441dc51d4a47bebc91e149398
Intisar OULBANI <ioulbani@in.karavel.com>
2023-03-02 17:48:09 +0100

    Update scripts && jenkinsfile

--- cypress/e2e/forgotPassword.cy.js
+++ cypress/e2e/forgotPassword.cy.js
@@ -3,7 +3,7 @@ describe('Forgot password', () => {
     it('I open login page', () => {
         cy.visit('http://zero.webappsecurity.com/login.html')
         cy.get('.offset3 > a').click()
-        cy.get('h3')&&cy.get('.btn').should('contain', 'Password')
-        //cy.get('.btn').should('contain', 'Password')
+        cy.get('h3').should('contain', 'Password')
+        cy.get('.btn').should('contain', 'Password')
     })
     })
